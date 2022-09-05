3927cc2a91e74d05a6a18b8e4eb82245683405e6
Alberto Amador <95782430+AmadorCEO@users.noreply.github.com>
2022-09-04 19:53:32 -0500

    Add files via upload

---
+++ TrabajoCocheCRUD/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        CocheCRUD cocheCrud = new CocheCRUDImpl();
+
+        Coche bmw = new Coche("Bmw","X4",2016);
+
+
+        System.out.println(bmw);
+
+    }
+}
